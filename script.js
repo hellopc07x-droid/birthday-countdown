80b3450db4b376e82123e8120c1c01285a28fea8
hellopc07x-droid <hello.pc.07x@gmail.com>
2026-08-23 05:41:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // ⚠️ Yahan birthday ki exact date & time set karo
 // Format: YYYY-MM-DDTHH:MM:SS
-const birthday = new Date("2026-08-24T18:15:00").getTime();
+const birthday = new Date("2026-08-24T00:00:00").getTime();
 
 
 // ==========================================
